1d7c5865caec73a5d47d162899c75faac5802e82
chenxiaojie <chenjie003@qq.com>
2020-02-22 10:29:12 +0800

    add vc

--- CJMultiLanguageDemo/CJMultiLanguageDemo/ViewController.m
+++ CJMultiLanguageDemo/CJMultiLanguageDemo/ViewController.m
@@ -7,18 +7,33 @@
 //
 
 #import "ViewController.h"
-#import "HMTabBarController.h"
+#import "Masonry.h"
 #import "AppDelegate.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UILabel *testLabel;
 
 @end
 
 @implementation ViewController
 
+- (UILabel *)testLabel {
+    if (_testLabel == nil) {
+        _testLabel = [[UILabel alloc] init];
+    }
+    return _testLabel;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.view addSubview:self.testLabel];
+    self.testLabel.text = @"测试文案";
+    
+    [self.testLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.mas_equalTo(self.view);
+    }];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
